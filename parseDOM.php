c7f699f0beaa2e11d2422ebaca9976091fb4b68b
Hartmut Beyer <hartmut_beyer@gmx.de>
2016-02-11 16:19:31 +0100

    Geodatenarchivierung verbessert

---
+++ parseDOM.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+function getTextContent($nodeList) {
+	$resultArray = array();
+	foreach($nodeList as $node) {
+		$resultArray[] = $node->textContent;
+	}
+	if(isset($resultArray[0]) and isset($resultArray[1])) {
+		return(implode('|', $resultArray));
+	}
+	if(isset($resultArray[0])) {
+		return($resultArray[0]);
+	}
+}
+
+function getAttributeFromNodeList($nodeList, $attribute) {
+	$resultArray = array();
+	foreach($nodeList as $node) {
+		$resultArray[] = $node->getAttribute($attribute);
+	}
+	if(isset($resultArray[0]) and isset($resultArray[1])) {
+		return(implode('|', $resultArray));
+	}
+	if(isset($resultArray[0])) {
+		return($resultArray[0]);
+	}
+}
+
+?>
