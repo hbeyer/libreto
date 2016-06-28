f70133652b4f65940adba5568276cc2d5e5af73a
Hartmut Beyer <beyer@hab.de>
2016-06-28 23:07:52 +0200

    Settings geleert

--- settings.php
+++ settings.php
@@ -1,9 +1,8 @@
-<?php
+﻿<?php
 
 $userGeoNames = 'hbeyer';
-$userAgentHTTP = 'Dr. Hartmut Beyer, Wolfenbüttel';
+$userAgentHTTP = 'Dr. Hartmut Beyer, HAB';
 
 date_default_timezone_set('Europe/Amsterdam');
-ini_set("max_execution_time", 300);
 
 ?>
