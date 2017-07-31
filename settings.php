31dd8caae153c5491ea1c21fdbbef19423d7dc8c
Hartmut Beyer <hartmut_beyer@gmx.de>
2017-07-31 14:31:03 +0200

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,7 +1,7 @@
 <?php
 
 $userGeoNames = '{login}'; // User name for http://www.geonames.org/login
-$userAgentHTTP = ''; // Name to use in HTTP header for downloading BEACON files
+$userAgentHTTP = '{name}'; // Name to use in HTTP header for downloading BEACON files
 
 date_default_timezone_set('Europe/Amsterdam');
 ini_set("max_execution_time", 300);
